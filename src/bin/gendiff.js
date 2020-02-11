9d1474817b731a82ebe65f6aada3163116ee889a
wesydi <marsss278@gmail.com>
2020-02-11 23:44:36 +0300

    add genDiff function

---
+++ src/bin/gendiff.js
@@ -0,0 +1,20 @@
+import gendiff from '..';
+
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(
+    (beforeConfig, afterConfig) => {
+      const before = JSON.parse(fs.readFileSync(`${path.resolve(process.cwd(), beforeConfig)}`));
+      const after = JSON.parse(fs.readFileSync(`${path.resolve(process.cwd(), afterConfig)}`));
+      console.log(gendiff(before, after));
+    },
+  );
+
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { uniq, has } from 'lodash';
+
+const genDiff = (firstConfig, secondConfig) => {
+  const keys = uniq(
+    [...Object.keys(firstConfig), ...Object.keys(secondConfig)].sort(),
+  );
+  const result = keys.reduce((acc, key) => {
+    if (firstConfig[key] === secondConfig[key]) acc.push(`   ${key}: ${firstConfig[key]}\n`);
+    if (!has(secondConfig, key)) acc.push(` - ${key}: ${firstConfig[key]}\n`);
+    if (!has(firstConfig, key)) acc.push(` + ${key}: ${secondConfig[key]}\n`);
+    if (
+      firstConfig[key] !== secondConfig[key]
+      && has(secondConfig, key)
+      && has(firstConfig, key)
+    ) {
+      acc.push(` - ${key}: ${firstConfig[key]}\n`);
+      acc.push(` + ${key}: ${secondConfig[key]}\n`);
+    }
+    return acc;
+  }, ['{\n']);
+  result.push('}');
+  return result.join('');
+};
+
+export default genDiff;
