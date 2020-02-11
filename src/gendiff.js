6e7887f864ae9c601a98f724614a323e06e2753f
wesydi <marsss278@gmail.com>
2020-02-11 23:44:36 +0300

    add genDiff function

--- src/gendiff.js
+++
@@ -1,9 +0,0 @@
-const program = require('commander');
-
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format');
-
-program.parse(process.argv);
