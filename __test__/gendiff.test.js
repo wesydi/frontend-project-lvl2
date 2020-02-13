6cedec584bb6cdd6dd910beb9c4e447fd4c706ce
wesydi <marsss278@gmail.com>
2020-02-13 21:55:18 +0300

    add module difference

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
-import gendiff from '../src/bin/gendiff';
+import difference from '../src/difference';
 
 const diff = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString();
 
 test('relative path', () => {
   const before = `${__dirname}/../__fixtures__/before.json`;
   const after = `${__dirname}/../__fixtures__/after.json`;
-  expect(gendiff(before, after)).toEqual(diff);
+  expect(difference(before, after)).toEqual(diff);
 });
