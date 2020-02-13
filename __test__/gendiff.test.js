bcc07906a7c458d6b79b57d91a34dde2d65b77e0
wesydi <marsss278@gmail.com>
2020-02-14 00:21:45 +0300

    add ini files

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -2,15 +2,23 @@ import fs from 'fs';
 import difference from '../src/difference';
 
 const diff = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString();
+const config1 = (format) => `${__dirname}/../__fixtures__/before${format}`;
+const config2 = (format) => `${__dirname}/../__fixtures__/after${format}`;
 
 test('json', () => {
-  const before = `${__dirname}/../__fixtures__/before.json`;
-  const after = `${__dirname}/../__fixtures__/after.json`;
+  const before = config1('.json');
+  const after = config2('.json');
   expect(difference(before, after)).toEqual(diff);
 });
 
 test('yml', () => {
-  const before = `${__dirname}/../__fixtures__/before.yml`;
-  const after = `${__dirname}/../__fixtures__/after.yml`;
+  const before = config1('.yml');
+  const after = config2('.yml');
+  expect(difference(before, after)).toEqual(diff);
+});
+
+test('ini', () => {
+  const before = config1('.ini');
+  const after = config2('.ini');
   expect(difference(before, after)).toEqual(diff);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 
 const parser = (filepath) => {
   const format = path.extname(filepath);
-  const data = fs.readFileSync(`${path.resolve(process.cwd(), filepath)}`);
+  const data = fs.readFileSync(`${path.resolve(process.cwd(), filepath)}`, 'utf-8');
   let parse;
   if (format === '.json') {
     parse = JSON.parse(data);
   } else if (format === '.yml') {
     parse = yaml.safeLoad(data);
+  } else if (format === '.ini') {
+    parse = ini.parse(data);
   }
   return parse;
 };
