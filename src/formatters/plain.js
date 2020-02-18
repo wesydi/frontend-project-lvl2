a92d93fdea670de8b0ea1acdc3369897677e8cc6
wesydi <marsss278@gmail.com>
2020-02-18 17:58:56 +0300

    add ancestry to plain func

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,20 +4,21 @@ const stringify = (element) => (element instanceof Object ? '[complex value]' : 
 
 const plain = (beforeConfig, afterConfig) => {
   const data = genDiff(beforeConfig, afterConfig);
-  const iter = (dataChildren, fullName) => {
+  const iter = (dataChildren, ancestry) => {
     const keys = Object.keys(dataChildren);
     const result = keys.reduce((acc, key) => {
       const {
         name, status, type, value, valuePrevious, children,
       } = dataChildren[key];
       if (type === 'obj') {
-        acc.push(`${children.map((el) => iter(el, [...fullName, name]))}`);
-      }
-      if (status === 'added') acc.push(`Property ${fullName.join('.')} was added with value: '${stringify(value)}'\n`);
-      if (status === 'deleted') acc.push(`Property ${fullName.join('.')} was deleted\n`);
+        return [...acc, children.map((el) => iter(el, [...ancestry, name]))]
+      };
+      const fullName = ancestry ? [...ancestry, name].join('.') : name;
+      if (status === 'added') acc.push(`Property ${fullName} was added with value: '${stringify(value)}'\n`);
+      if (status === 'deleted') acc.push(`Property ${fullName} was deleted\n`);
       if (status === 'edited' && type !== 'obj') acc.push(`Property ${fullName} was changed from '${stringify(valuePrevious)}' to '${stringify(value)}'\n`);
       return acc;
-    }, ['']);
+    }, []);
     return result.join('');
   };
   return iter(data, []);
