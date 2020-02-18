75f6085ace0b36ca3f0c476a5545590a0fad9f0b
wesydi <marsss278@gmail.com>
2020-02-18 23:54:39 +0300

    refactor test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,9 +1,7 @@
 import fs from 'fs';
 import difference from '../src';
 
-const diffRecursive = fs.readFileSync(`${__dirname}/../__fixtures__/result_recursive.txt`, 'utf-8');
-const diffPlain = fs.readFileSync(`${__dirname}/../__fixtures__/result_plain.txt`, 'utf-8');
-const diffJson = fs.readFileSync(`${__dirname}/../__fixtures__/result_json.txt`, 'utf-8');
+const diff = (typeResult) => fs.readFileSync(`${__dirname}/../__fixtures__/result_${typeResult}.txt`, 'utf-8');
 const config1 = (format) => `${__dirname}/../__fixtures__/before${format}`;
 const config2 = (format) => `${__dirname}/../__fixtures__/after${format}`;
 
@@ -11,19 +9,19 @@ describe('nested format', () => {
   test('json', () => {
     const before = config1('.json');
     const after = config2('.json');
-    expect(difference(before, after)).toEqual(diffRecursive);
+    expect(difference(before, after)).toEqual(diff('nested'));
   });
 
   test('yml', () => {
     const before = config1('.yml');
     const after = config2('.yml');
-    expect(difference(before, after)).toEqual(diffRecursive);
+    expect(difference(before, after)).toEqual(diff('nested'));
   });
 
   test('ini', () => {
     const before = config1('.ini');
     const after = config2('.ini');
-    expect(difference(before, after)).toEqual(diffRecursive);
+    expect(difference(before, after)).toEqual(diff('nested'));
   });
 });
 
@@ -31,19 +29,19 @@ describe('plain format', () => {
   test('json', () => {
     const before = config1('.json');
     const after = config2('.json');
-    expect(difference(before, after, 'plain')).toEqual(diffPlain);
+    expect(difference(before, after, 'plain')).toEqual(diff('plain'));
   });
 
   test('yml', () => {
     const before = config1('.yml');
     const after = config2('.yml');
-    expect(difference(before, after, 'plain')).toEqual(diffPlain);
+    expect(difference(before, after, 'plain')).toEqual(diff('plain'));
   });
 
   test('ini', () => {
     const before = config1('.ini');
     const after = config2('.ini');
-    expect(difference(before, after, 'plain')).toEqual(diffPlain);
+    expect(difference(before, after, 'plain')).toEqual(diff('plain'));
   });
 });
 
@@ -51,18 +49,18 @@ describe('json format', () => {
   test('json', () => {
     const before = config1('.json');
     const after = config2('.json');
-    expect(difference(before, after, 'json')).toEqual(diffJson);
+    expect(difference(before, after, 'json')).toEqual(diff('json'));
   });
 
   test('yml', () => {
     const before = config1('.yml');
     const after = config2('.yml');
-    expect(difference(before, after, 'json')).toEqual(diffJson);
+    expect(difference(before, after, 'json')).toEqual(diff('json'));
   });
 
   test('ini', () => {
     const before = config1('.ini');
     const after = config2('.ini');
-    expect(difference(before, after, 'json')).toEqual(diffJson);
+    expect(difference(before, after, 'json')).toEqual(diff('json'));
   });
 });
