35f06c55f853c7a8a1db05bb8d027f4ef58e4d5b
wesydi <marsss278@gmail.com>
2020-02-18 21:26:10 +0300

    rename function

---
+++ src/formatters/nested.js
@@ -0,0 +1,29 @@
+import { genDiff, stringify } from '..';
+
+const recursive = (beforeConfig, afterConfig) => {
+  const data = genDiff(beforeConfig, afterConfig);
+  const space = ' ';
+  const iter = (dataChildren) => {
+    const keys = Object.keys(dataChildren);
+    const result = keys.reduce((acc, key) => {
+      const {
+        name, status, type, value, valuePrevious, children,
+      } = dataChildren[key];
+      if (type === 'obj') {
+        acc.push(`${space.repeat(3)}${name}: ${children.map((el) => iter(el))}\n`);
+      }
+      if (status === 'unchanged') acc.push(`${space.repeat(4)}   ${name}: ${stringify(value)}\n`);
+      if (status === 'added') acc.push(`${space.repeat(4)} + ${name}: ${stringify(value)}\n`);
+      if (status === 'deleted') acc.push(`${space.repeat(4)} - ${name}: ${stringify(valuePrevious)}\n`);
+      if (status === 'edited' && type !== 'obj') {
+        acc.push(`${space.repeat(4)} - ${name}: ${stringify(valuePrevious)}\n`);
+        acc.push(`${space.repeat(4)} + ${name}: ${stringify(value)}\n`);
+      }
+      return acc;
+    }, ['{\n']);
+    result.push('}');
+    return result.join('');
+  };
+  return iter(data);
+};
+export default recursive;
--- src/render.js
+++ src/render.js
@@ -1,10 +1,10 @@
-import recursive from './formatters/recursive';
+import toNested from './formatters/nested';
 import toPlain from './formatters/plain';
 import toJson from './formatters/json';
 
 const render = (config1, config2, format) => {
   if (format === 'plain') return toPlain(config1, config2, format);
   if (format === 'json') return toJson(config1, config2, format);
-  return recursive(config1, config2);
+  return toNested(config1, config2);
 };
 export default render;
