a3da27561f977e5111aafe15bfa636a0d084e483
wesydi <marsss278@gmail.com>
2020-02-24 19:18:10 +0300

    refactor test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,26 +5,15 @@ import difference from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const cases = (beforeConfig, afterConfig, result) => [[getFixturePath(`${beforeConfig}`), getFixturePath(`${afterConfig}`), readFile(`${result}`)]];
+const cases = [['before.json', 'after.yml', 'nested', 'result_nested.txt'], ['before.ini', 'after.yml', 'plain', 'result_plain.txt'], ['before.json', 'after.ini', 'json', 'result_json.txt']];
 
-test.each(cases('before.json', 'after.yml', 'result_nested.txt'))(
-  'format nested',
-  (firstArg, secondArg, expectedResult) => {
-    const result = difference(firstArg, secondArg);
-    expect(result).toEqual(expectedResult);
-  },
-);
-test.each(cases('before.ini', 'after.yml', 'result_plain.txt'))(
-  'format plain',
-  (firstArg, secondArg, expectedResult) => {
-    const result = difference(firstArg, secondArg, 'plain');
-    expect(result).toEqual(expectedResult);
-  },
-);
-test.each(cases('before.json', 'after.ini', 'result_json.txt'))(
-  'format json',
-  (firstArg, secondArg, expectedResult) => {
-    const result = difference(firstArg, secondArg, 'json');
-    expect(result).toEqual(expectedResult);
+test.each(cases)(
+  'format nested, plain, json',
+  (firstArg, secondArg, format, expectedResult) => {
+    const beforeConfig = getFixturePath(firstArg);
+    const afterConfig = getFixturePath(secondArg);
+    const expected = readFile(expectedResult);
+    const result = difference(beforeConfig, afterConfig, format);
+    expect(result).toEqual(expected);
   },
 );
