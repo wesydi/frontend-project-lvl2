4038449bf1eea7e6d7bf0b824f0897d4326b6ad0
wesydi <marsss278@gmail.com>
2020-03-09 21:08:58 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -8,25 +8,25 @@ import toJson from './formatters/json';
 const readFile = (filepath) => fs.readFileSync(`${path.resolve(process.cwd(), filepath)}`, 'utf-8');
 const extensionFile = (filepath) => path.extname(filepath);
 
-const render = (config1, config2, format) => {
+const render = (beforeConfig, afterConfig, format) => {
   switch (format) {
     case 'nested':
-      return toNested(config1, config2);
+      return toNested(beforeConfig, afterConfig);
     case 'plain':
-      return toPlain(config1, config2, format);
+      return toPlain(beforeConfig, afterConfig, format);
     case 'json':
-      return toJson(config1, config2, format);
+      return toJson(beforeConfig, afterConfig, format);
     default: throw new Error(`Unknown format: '${format}'!`);
   }
 };
 
 const genDifference = (pathToFirstFile, pathToSecondFile, format = 'nested') => {
   const extensionFirstFile = extensionFile(pathToFirstFile);
-  const extensionSecondFile = extensionFile(pathToSecondFile);
   const dataFirstFile = readFile(pathToFirstFile);
+  const beforeConfig = parse(extensionFirstFile, dataFirstFile);
+  const extensionSecondFile = extensionFile(pathToSecondFile);
   const dataSecondFile = readFile(pathToSecondFile);
-  const before = parse(extensionFirstFile, dataFirstFile);
-  const after = parse(extensionSecondFile, dataSecondFile);
-  return render(before, after, format);
+  const afterConfig = parse(extensionSecondFile, dataSecondFile);
+  return render(beforeConfig, afterConfig, format);
 };
 export default genDifference;
