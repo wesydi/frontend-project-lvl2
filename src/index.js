d0e835e26220becdfe397c60039e5673253579cc
wesydi <marsss278@gmail.com>
2020-03-21 01:46:09 +0300

    add render to index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
-import toNested from './formatters/nested';
-import toPlain from './formatters/plain';
-import toJson from './formatters/json';
+import render from './formatters'
 
 const readFile = (filepath) => fs.readFileSync(`${path.resolve(process.cwd(), filepath)}`, 'utf-8');
 const extensionFile = (filepath) => path.extname(filepath);
-
-const render = (beforeConfig, afterConfig, format) => {
-  switch (format) {
-    case 'nested':
-      return toNested(beforeConfig, afterConfig);
-    case 'plain':
-      return toPlain(beforeConfig, afterConfig, format);
-    case 'json':
-      return toJson(beforeConfig, afterConfig, format);
-    default: throw new Error(`Unknown format: '${format}'!`);
-  }
-};
 
 const genDifference = (pathToFirstFile, pathToSecondFile, format = 'nested') => {
   const typeFirstFile = extensionFile(pathToFirstFile).slice(1);
@@ -29,4 +15,5 @@ const genDifference = (pathToFirstFile, pathToSecondFile, format = 'nested') => 
   const afterConfig = parse(typeSecondFile, dataSecondFile);
   return render(beforeConfig, afterConfig, format);
 };
+
 export default genDifference;
