c3c81a180a25b6b79b43ca30bca0fb2e664de6c0
wesydi <marsss278@gmail.com>
2020-03-21 02:44:53 +0300

    refactor format plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,26 +6,26 @@ const plain = (beforeConfig, afterConfig) => {
   const data = genAST(beforeConfig, afterConfig);
   const iter = (dataChildren, ancestry) => {
     const keys = Object.keys(dataChildren);
-    const result = keys.map((key) => {
-      const {
-        name, status, value, valuePrevious, children,
-      } = dataChildren[key];
-      if (children) {
-        return children.map((el) => iter(el, [...ancestry, name]));
-      }
-      const fullName = ancestry ? [...ancestry, name].join('.') : name;
-      switch (status) {
-        case 'added':
-          return `Property ${fullName} was added with value: '${stringify(value)}'\n`;
-        case 'deleted':
-          return `Property ${fullName} was deleted\n`;
-        case 'edited':
-          return `Property ${fullName} was changed from '${stringify(valuePrevious)}' to '${stringify(value)}'\n`;
-        case 'unchanged':
-          return '';
-        default: throw new Error(`Unknown status: '${status}'!`);
-      }
-    });
+    const result = keys
+      .filter((key) => dataChildren[key].status !== 'unchanged')
+      .map((key) => {
+        const {
+          name, status, value, valuePrevious, children,
+        } = dataChildren[key];
+        if (children) {
+          return children.map((el) => iter(el, [...ancestry, name]));
+        }
+        const fullName = ancestry ? [...ancestry, name].join('.') : name;
+        switch (status) {
+          case 'added':
+            return `Property ${fullName} was added with value: '${stringify(value)}'\n`;
+          case 'deleted':
+            return `Property ${fullName} was deleted\n`;
+          case 'edited':
+            return `Property ${fullName} was changed from '${stringify(valuePrevious)}' to '${stringify(value)}'\n`;
+          default: throw new Error(`Unknown status: '${status}'!`);
+        }
+      });
     return result.join('');
   };
   return iter(data, []);
