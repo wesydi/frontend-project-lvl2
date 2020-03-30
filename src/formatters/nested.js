e7ce3127bbb8b4c172f1beecc28f825d868da228
wesydi <marsss278@gmail.com>
2020-03-30 23:34:03 +0300

    fix spaces

--- src/formatters/nested.js
+++ src/formatters/nested.js
@@ -1,36 +1,36 @@
-const space = ' ';
+const space = (n) => ' '.repeat(n);
 
-const stringify = (element) => {
+const stringify = (element, depth, numberSpace) => {
   if (!(element instanceof Object)) return element;
   const keys = Object.keys(element);
-  const result = keys.map((key) => `{\n${space.repeat(10)}${key}: ${element[key]}\n`);
-  return [...result, `${space.repeat(5)}}`].join('');
+  const result = keys.map((key) => `{\n${space(numberSpace * depth + 5)}${key}: ${element[key]}\n`);
+  return [...result, `${space(numberSpace * depth + 2)}}`].join('');
 };
 
 const nested = (AST) => {
-  const iter = (dataChildren) => {
+  const iter = (dataChildren, depth, numberSpace) => {
     const keys = Object.keys(dataChildren);
     const result = keys.map((key) => {
       const {
         name, status, value, valuePrevious, children,
       } = dataChildren[key];
       if (children) {
-        return `${space.repeat(3)}${name}: ${children.map(iter)}\n`;
+        return `${space(numberSpace * depth + 2)}${name}: ${children.map((x) => iter(x, depth + 1, numberSpace * depth + 2))}\n`;
       }
       switch (status) {
         case 'unchanged':
-          return `${space.repeat(7)}${name}: ${stringify(value)}\n`;
+          return `${space(numberSpace + 2)}${name}: ${stringify(value, depth, numberSpace)}\n`;
         case 'added':
-          return `${space.repeat(5)}+ ${name}: ${stringify(value)}\n`;
+          return `${space(numberSpace)}+ ${name}: ${stringify(value, depth, numberSpace)}\n`;
         case 'deleted':
-          return `${space.repeat(5)}- ${name}: ${stringify(valuePrevious)}\n`;
+          return `${space(numberSpace)}- ${name}: ${stringify(valuePrevious, depth, numberSpace)}\n`;
         case 'edited':
-          return `${space.repeat(5)}- ${name}: ${stringify(valuePrevious)}\n${space.repeat(4)} + ${name}: ${stringify(value)}\n`;
+          return `${space(numberSpace)}- ${name}: ${stringify(valuePrevious, depth, numberSpace)}\n${space(numberSpace - 1)} + ${name}: ${stringify(value, depth, numberSpace)}\n`;
         default: throw new Error(`Unknown status: '${status}'!`);
       }
     });
-    return ['{\n', ...result, '}'].join('');
+    return ['{\n', ...result, space(numberSpace * depth), '}'].join('');
   };
-  return iter(AST);
+  return iter(AST, 0, 5);
 };
 export default nested;
